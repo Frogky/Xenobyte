c792506039ec646753e5d2f6779aee8375dfc084
Frogky <139832439+Frogky@users.noreply.github.com>
2025-03-01 03:28:06 -0500

    Update CarpenterUse.java

--- src/main/java/forgefuck/team/xenobyte/modules/CarpenterUse.java
+++ src/main/java/forgefuck/team/xenobyte/modules/CarpenterUse.java
@@ -8,22 +8,32 @@ import forgefuck.team.xenobyte.api.module.PerformMode;
 import net.minecraftforge.client.event.MouseEvent;
 
 public class CarpenterUse extends CheatModule {
-    
+
     public CarpenterUse() {
         super("CarpenterUse", Category.MODS, PerformMode.TOGGLE);
     }
-    
-    @SubscribeEvent public void mouseEvent(MouseEvent e) {
+
+    @SubscribeEvent
+    public void mouseEvent(MouseEvent e) {
         if (e.button == 1 && e.buttonstate) {
             int[] mop = utils.mop();
-            utils.sendPacket("CarpentersBlocks", 0, mop[0], mop[1], mop[2], mop[3]);
+            // Try different packet IDs here.  Start with 1, then 2, etc.
+            utils.sendPacket("CarpentersBlocks", 1, mop[0], mop[1], mop[2], mop[3]); 
             e.setCanceled(true);
         }
     }
-    
-    @Override public String moduleDesc() {
+
+    @Override
+    public String moduleDesc() {
         return lang.get("In most cases, allows you to use right click in clamed area", "В большинстве случаев позволяет использовать ПКМ в привате");
     }
+
+    @Override
+    public boolean isWorking() {
+        return Loader.isModLoaded("CarpentersBlocks");
+    }
+
+}
 
     @Override public boolean isWorking() {
         return Loader.isModLoaded("CarpentersBlocks");
